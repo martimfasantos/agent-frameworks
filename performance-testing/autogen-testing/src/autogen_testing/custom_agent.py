6ab8b61a6847efea65e17ff92a2c01768cb37245
martimfasantos <martimfasantos@gmail.com>
2025-01-28 16:12:28 +0000

    clean up

--- performance-testing/autogen-testing/src/autogen_testing/custom_agent.py
+++ performance-testing/autogen-testing/src/autogen_testing/custom_agent.py
@@ -1,25 +1,6 @@
-from typing import AsyncGenerator, List, Sequence, Tuple, Callable
-from urllib import response
 from llama_index.core.agent import ReActAgent
 from autogen_agentchat.agents import BaseChatAgent, AssistantAgent
-from autogen_agentchat.base import Response
-from autogen_agentchat.conditions import MaxMessageTermination
-from autogen_agentchat.messages import ChatMessage, TextMessage
 from autogen_core.tools import FunctionTool
-from autogen_agentchat.teams import SelectorGroupChat
-from autogen_agentchat.ui import Console
-from autogen_core import CancellationToken
-from autogen_agentchat.messages import HandoffMessage, ToolCallSummaryMessage
-from rich.console import Console
-from rich.markdown import Markdown
-from autogen_core.models import (
-    AssistantMessage,
-    ChatCompletionClient,
-    FunctionExecutionResultMessage,
-    UserMessage,
-    SystemMessage
-)
-
 
 class DatabaseRetrieverAgent(AssistantAgent):
     def __init__(self, *args, react_agent: ReActAgent, **kwargs) -> None:
