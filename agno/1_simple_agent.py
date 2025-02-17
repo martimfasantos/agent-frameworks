de283b4b6b0c0b611304f46e7a3e798111f16f9b
martimfasantos <72747170+martimfasantos@users.noreply.github.com>
2025-02-17 18:42:37 +0000

    added agno examples templates

---
+++ agno/1_simple_agent.py
@@ -0,0 +1,22 @@
+from agno.agent import Agent
+from agno.models.openai import OpenAIChat
+from agno.tools.duckduckgo import DuckDuckGoTools
+from settings import settings
+
+# In this example, we explore Agno's Agent class with the following features:
+# - Using tools
+# - Pretty-printing responses
+
+agent = Agent(
+    model=OpenAIChat(
+        id=settings.openai_model_name,
+        api_key=settings.openai_api_key.get_secret_value(),
+    ),
+    tools=[DuckDuckGoTools()],
+    description="You are an enthusiastic news reporter with a flair for storytelling!",
+    show_tool_calls=True,
+    markdown=True,
+)
+
+agent.print_response("Tell me about a breaking news story from New York.", stream=False)
+
---
+++ agno/2_async_and_streaming.py
@@ -0,0 +1,31 @@
+import asyncio
+from agno.agent import Agent
+from agno.models.openai import OpenAIChat
+from agno.tools.duckduckgo import DuckDuckGoTools
+from settings import settings
+
+# In this example, we explore Agno's Agent class with the following features:
+# - Asynchronous runs
+# - Streaming responses
+
+async def main():
+
+    agent = Agent(
+        model=OpenAIChat(
+            id=settings.openai_model_name,
+            api_key=settings.openai_api_key.get_secret_value(),
+        ),
+        tools=[DuckDuckGoTools()],
+        description="You are an enthusiastic news reporter with a flair for storytelling!",
+        show_tool_calls=True,
+        markdown=True,
+    )
+
+    # result = await agent.arun("Tell me about a breaking news story from New York.", stream=False)
+    result = await agent.arun("Tell me about a breaking news story from New York.", stream=True)
+    async for r in result:
+        print(r.content) # Print the tokens - results yielded are RunResponse() dataclass objects
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
